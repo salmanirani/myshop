45c7bdd68d2ac272e6ebd470ed2c7b76c2f21cdb
salmanirani <irani.salman@gmail.com>
2022-12-31 10:39:09 +0330

    upload app and packaje.json

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Google\Service\ShoppingContent\Resource\Pos;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::with('user')->get();
+        return view('frontend.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post = new Post();
+        $post->title = $request->title;
+        $post->title_en = $request->title_en;
+        $post->description = $request->description;
+        $post->user_id = 1;
+        $post->save();
+        Session::flash('add_post', 'با موفقیت درج شد');
+        return redirect('post');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $post = Post::where('id', $id)->first();
+        return view('frontend.posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $post = Post::findOrfail($id);
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->title_en = $request->title_en;
+        $post->save();
+
+        Session::flash('add_post', 'با موفقیت ویراش شد');
+        return redirect('post');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+        Session::flash('delete_post', 'با موفقیت حذف شد');
+        return redirect('post');
+    }
+}
---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ShopController extends Controller
+{
+    public function showMyPage($name)
+    {
+        echo 'My shop page  '.$name;
+
+    }
+}
